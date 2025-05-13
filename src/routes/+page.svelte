dbac6b5205bd04f6e45ed9283691fa20781f4c07
Advay Patil <advaypatil27@gmail.com>
2025-05-12 22:53:48 -0700

    Clean up files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,3 @@
-<script>
-  import { goto } from '$app/navigation';
-
-</script>
-
 <div class="flex flex-col justify-center items-center h-screen">
-  <h1 class="text-4xl font-bold mb-6 text-center">
-    Resume Review
-  </h1>
-  <button 
-    class="w-48 bg-blue-500 text-white rounded-lg px-6 py-3 text-lg font-medium hover:bg-blue-700 transition duration-300 mt-4 cursor-pointer"
-    on:click={() => {
-      goto("/login");
-    }}
-  >
-    Login
-  </button>
-  <button 
-    class="w-48 bg-blue-500 text-white rounded-lg px-6 py-3 text-lg font-medium hover:bg-blue-700 transition duration-300 mt-4 cursor-pointer"
-    on:click={() => {
-      goto("/register");
-    }}
-  >
-    Register
-  </button>
+    <h1 class="text-4xl font-bold mb-6 text-center">Resume Review</h1>
 </div>
--- src/routes/app/+layout.server.ts
+++ src/routes/app/+layout.server.ts
@@ -1,18 +1,23 @@
-import { redirect } from '@sveltejs/kit';
-import { LOCAL_API_ENDPOINT, PROD_API_ENDPOINT } from '../../constants';
+import { redirect } from "@sveltejs/kit";
+import { LOCAL_API_ENDPOINT, PROD_API_ENDPOINT } from "../../constants";
+import type { LoadEvent } from "@sveltejs/kit";
+import { devPrint } from "../../utils";
+import type { Member } from "../../types";
 
-export async function load({ fetch }) {
-  const baseURL = import.meta.env.DEV ? LOCAL_API_ENDPOINT : PROD_API_ENDPOINT;
-  const response = await fetch(`${baseURL}/members/profile`);
+export async function load({ fetch }: LoadEvent) {
+    const baseURL = import.meta.env.DEV
+        ? LOCAL_API_ENDPOINT
+        : PROD_API_ENDPOINT;
+    const response = await fetch(`${baseURL}/members/profile`);
 
-  if (!response.ok) {
-    throw redirect(302, '/login');
-  }
+    if (!response.ok) {
+        throw redirect(302, "/login");
+    }
 
-  const data = await response.json();
-  console.log('User Data: ', data);
+    const data = await response.json();
+    devPrint("User Data: ", data);
 
-  return {
-    user: data,
-  };
+    return {
+        user: data as Member,
+    };
 }
